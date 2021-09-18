104289893362e6efa40ad777bf90592321f3e4cf
aNhdUy13 <73863929+aNhdUy13@users.noreply.github.com>
2021-09-18 08:51:40 +0700

    import npm & mongo library

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+app.set('view engine', 'hbs');
+
+
+
+var bodyParser = require("body-parser");
+//const { Console } = require('console');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.get('/', (req, res) => {
+    res.render('main');
+})
+
+
+/* Regarding Css */
+app.use(express.static('public'));
+
+/* (End) Regarding Css */
+
+
+const PORT = 5000;
+app.listen(process.env.PORT || PORT);
+console.log("Console ATN web is running !");
