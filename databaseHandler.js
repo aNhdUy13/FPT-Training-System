3d037174382fef1721d5d085a4c3129341183022
Duy Anh Nguyen <73863929+aNhdUy13@users.noreply.github.com>
2021-09-27 20:47:54 +0700

    update staff

--- databaseHandler.js
+++ databaseHandler.js
@@ -76,8 +76,11 @@ async function searchTraineeAccount(collectionName, traineeNameAgeSearch) {
     const dbo = await getDBO();
 
     // search gần đúng (var re = /\w+\s/g ; g = global ; )
-    //const searchCondition = new RegExp(searchText, 'i') // i = case-insensitive (k phân biệt chữ hoa thường)
+    //const searchCondition = new RegExp(collectionName, 'i') // i = case-insensitive (k phân biệt chữ hoa thường)
     //const result = await dbo.collection(collectionName).find({ name: nameSearch }).toArray();
+    // const result = await dbo.collection(collectionName).find({
+    //     $or: [{ name: new RegExp(traineeNameAgeSearch, 'i') }, { age: traineeNameAgeSearch }]
+    // }).toArray();
     const result = await dbo.collection(collectionName).find({
         $or: [{ name: traineeNameAgeSearch }, { age: traineeNameAgeSearch }]
     }).toArray();
