02c996f676b0399d5f1ee8e9af26ad8c7223d4c9
Duy Anh Nguyen <73863929+aNhdUy13@users.noreply.github.com>
2021-09-28 11:11:01 +0700

    update staff role

--- app.js
+++ app.js
@@ -44,93 +44,11 @@ app.use(express.static('public'));
 
 /* (End) Regarding Css */
 
-// Duy Anh
-app.post('/doAddTraineeAccount', async(req, res) => {
-
-    var traineeEmail = req.body.txtTraineeEmail;
-    var traineePassword = req.body.txtTraineePassword;
-    var traineeName = req.body.txtTraineeName;
-    var traineeAge = req.body.txtTraineeAge;
-    var traineeDoB = req.body.txtTraineeDoB;
-    var traineeEducation = req.body.txtTraineeEducation;
-
-    if (traineeName.trim().length < 5) {
-        res.render('staff/traineeManagement', { errorName: "Error : Name cannot lower than 5 " })
-
-    } else if (traineeEmail.trim().length == 0) {
-
-        res.render('staff/traineeManagement', { errorEmail: "Error : Fill the email " })
-    } else if (traineePassword.trim().length == 0) {
-        res.render('staff/traineeManagement', { errorPassword: "Error : Fill the password " })
-    } else if (traineeAge.trim().length == 0 || isNaN(traineeAge) == true) {
-
-        res.render('staff/traineeManagement', { errorAge: "Error : Fill the age and it must be integer " })
-    } else if (traineeAge < 0) {
-        res.render('staff/traineeManagement', { errorAge: "Error : Age cannot < 0 " })
-    } else {
-        await dbHandler.createTraineeAccount("users", traineeEmail, traineePassword, traineeName,
-            traineeAge, traineeDoB, traineeEducation);
-
-        res.redirect('staff/traineeManagement');
-    }
-
-
-})
-
-app.get('/updateTraineeAccount', async(req, res) => {
-
-    const id = req.query.id;
-
-    var traineeAccountToEdit = await dbHandler.updateTraineeAccount("users", id);
-    res.render('staff/updateTraineeAccount', { traineeDetail: traineeAccountToEdit })
-
-})
-app.post('/doUpdateTraineeAccount', async(req, res) => {
-    const id = req.body.id;
-    const nameUpdated = req.body.txtUpdateTraineeName;
-    const emailUpdated = req.body.txtUpdateTraineeEmail;
-    const ageUpdated = req.body.txtUpdateTraineeAge;
-    const dobUpdated = req.body.txtUpdateTraineeDoB;
-    const educationUpdated = req.body.txtUpdateTraineeEducation;
-
-
-    const newValue = {
-        $set: {
-            email: emailUpdated,
-            name: nameUpdated,
-            age: ageUpdated,
-            DoB: dobUpdated,
-            education: educationUpdated
-        }
-    };
-
-
-    await dbHandler.doUpdateTraineeAccount("users", id, newValue);
-
-    res.redirect('staff/traineeManagement')
-})
-
-app.get('/deleteTraineeAccount', async(req, res) => {
-    const id = req.query.id;
-
-    await dbHandler.deleteTraineeAccount("users", id);
-    res.redirect('staff/traineeManagement')
-})
-
-app.post('/searchTraineeAccount', async(req, res) => {
-    const traineeNameAgeSearch = req.body.txtTraineeNameAgeSearch;
-
-    const result = await dbHandler.searchTraineeAccount("users", traineeNameAgeSearch);
-
-    res.render('staff/traineeManagement', { viewAllTraineeAccount: result });
-})
-// Duy Anh
-
 // Hoang
 app.post('/addCourseCategory', async(req, res) => {
     const nameCourseCate = req.body.txtNameCourseCategory;
     const descriptionCourseCate = req.body.txtCourseDescription;
-    const dataCourseCategory = {name: nameCourseCate, description: descriptionCourseCate}
+    const dataCourseCategory = { name: nameCourseCate, description: descriptionCourseCate }
     await dbHandler.insertCourseCategory("courseCategory", dataCourseCategory);
 
     res.redirect('staff/CourseCategory');
@@ -143,13 +61,13 @@ app.get('/deleteCourseCategory', async(req, res) => {
     res.redirect('staff/CourseCategory')
 })
 app.post('/searchCourseCategory', async(req, res) => {
-    const nameCourseCate = req.body.txtNameCourseCategory;
+        const nameCourseCate = req.body.txtNameCourseCategory;
 
-    const result = await dbHandler.searchCourseCategory("courseCategory", nameCourseCate);
+        const result = await dbHandler.searchCourseCategory("courseCategory", nameCourseCate);
 
-    res.render('staff/CourseCategory', { viewAllCourseCategory: result });
-})
-// Hoang
+        res.render('staff/CourseCategory', { viewAllCourseCategory: result });
+    })
+    // Hoang
 
 const PORT = 5000;
 app.listen(process.env.PORT || PORT);
--- staff.js
+++ staff.js
@@ -17,45 +17,99 @@ router.get('/traineeManagement', async(req, res) => {
 })
 
 
-router.get('/updateTraineeAccount', (req, res) => {
 
-    res.render('staff/updateTraineeAccount');
+router.post('/doAddTraineeAccount', async(req, res) => {
+
+    var traineeEmail = req.body.txtTraineeEmail;
+    var traineePassword = req.body.txtTraineePassword;
+    var traineeName = req.body.txtTraineeName;
+    var traineeAge = req.body.txtTraineeAge;
+    var traineeDoB = req.body.txtTraineeDoB;
+    var traineeEducation = req.body.txtTraineeEducation;
+
+    if (traineeName.trim().length < 5) {
+        res.render('staff/traineeManagement', { errorName: "Error : Name cannot lower than 5 " })
+
+    } else if (traineeEmail.trim().length == 0) {
+
+        res.render('staff/traineeManagement', { errorEmail: "Error : Fill the email " })
+    } else if (traineePassword.trim().length == 0) {
+        res.render('staff/traineeManagement', { errorPassword: "Error : Fill the password " })
+    } else if (traineeAge.trim().length == 0 || isNaN(traineeAge) == true) {
+
+        res.render('staff/traineeManagement', { errorAge: "Error : Fill the age and it must be integer " })
+    } else if (traineeAge < 0) {
+        res.render('staff/traineeManagement', { errorAge: "Error : Age cannot < 0 " })
+    } else {
+        await dbHandler.createTraineeAccount("users", traineeEmail, traineePassword, traineeName,
+            traineeAge, traineeDoB, traineeEducation);
+
+        res.redirect('traineeManagement');
+    }
+
+
 })
 
+router.get('/updateTraineeAccount', async(req, res) => {
 
-// router.post('/doAddTraineeAccount', async(req, res) => {
+    const id = req.query.id;
 
-//     console.log("Add Trainee Account !");
+    var traineeAccountToEdit = await dbHandler.updateTraineeAccount("users", id);
+    res.render('staff/updateTraineeAccount', { traineeDetail: traineeAccountToEdit })
 
-//     var traineeEmail = req.body.txtTraineeEmail;
-//     var traineePassword = req.body.txtTraineePassword;
-//     var traineeName = req.body.txtTraineeName;
-//     var traineeAge = req.body.txtTraineeAge;
-//     var traineeDoB = req.body.txtTraineeDoB;
-//     var traineeEducation = req.body.txtTraineeEducation;
-
-//     await dbHandler.createTraineeAccount("users", traineeEmail, traineePassword, traineeName,
-//         traineeAge, traineeDoB, traineeEducation);
-
-//     res.redirect('/traineeManagement');
-
-// })
-// router.get('/deleteTraineeAccount', async(req, res) => {
-//     const id = req.query.id;
-
-//     await dbHandler.deleteTraineeAccount("users", id);
-//     res.redirect('/traineeManagement')
-// })
-router.get('/CourseCategory', async(req, res) => {
-    const result = await dbHandler.viewAllCourseCategory("courseCategory")
-
-    res.render('staff/CourseCategory', { viewAllCourseCategory: result });
 })
+router.post('/doUpdateTraineeAccount', async(req, res) => {
+    const id = req.body.id;
+    const nameUpdated = req.body.txtUpdateTraineeName;
+    const emailUpdated = req.body.txtUpdateTraineeEmail;
+    const ageUpdated = req.body.txtUpdateTraineeAge;
+    const dobUpdated = req.body.txtUpdateTraineeDoB;
+    const educationUpdated = req.body.txtUpdateTraineeEducation;
+
+
+    const newValue = {
+        $set: {
+            email: emailUpdated,
+            name: nameUpdated,
+            age: ageUpdated,
+            DoB: dobUpdated,
+            education: educationUpdated
+        }
+    };
+
+
+    await dbHandler.doUpdateTraineeAccount("users", id, newValue);
+
+    res.redirect('traineeManagement')
+})
+
+router.get('/deleteTraineeAccount', async(req, res) => {
+    const id = req.query.id;
+
+    await dbHandler.deleteTraineeAccount("users", id);
+    res.redirect('traineeManagement')
+})
+
+router.post('/searchTraineeAccount', async(req, res) => {
+    const traineeNameAgeSearch = req.body.txtTraineeNameAgeSearch;
+
+    const result = await dbHandler.searchTraineeAccount("users", traineeNameAgeSearch);
+
+    res.render('staff/traineeManagement', { viewAllTraineeAccount: result });
+})
+
 
 /**
  *  (END) Nguyen DUy Anh
  */
 
+
+
+router.get('/CourseCategory', async(req, res) => {
+    const result = await dbHandler.viewAllCourseCategory("courseCategory")
+
+    res.render('staff/CourseCategory', { viewAllCourseCategory: result });
+})
 
 
 
