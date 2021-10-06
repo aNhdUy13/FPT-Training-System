4bdd5f20aa9dd0963981efe839a004d43a155c58
hoang4326 <84314103+hoang4326@users.noreply.github.com>
2021-10-06 14:04:42 +0700

    Update trainee.js

--- trainee.js
+++ trainee.js
@@ -7,18 +7,18 @@ router.get('/', (req, res) => {
     res.render('trainer/trainerHome');
 })
 
-
-module.exports = router;
-
-
 router.post('/updateTraineeAccount',async (req,res)=>{
     const nameInput = req.body.txtName;
     const ageInput = req.body.txtAge;
     const dobInput = req.body.txtDoB;
     const educationInput = req.body.txtEducation;
-    const dbo = await dbHandler.GetDB();
     const newValues ={$set : {name: nameInput,age: ageInput, dob: dobInput, education: educationInput}};
     
-    await dbHandler.updateTraineeAccount("users", newValues);
+    await dbHandler.updateFunction("users", newValues);
     res.redirect('/view');
 })
+
+
+module.exports = router;
+
+
